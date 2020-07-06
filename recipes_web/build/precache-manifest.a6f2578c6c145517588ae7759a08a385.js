c7b18f1c29eb2aaf2c7d51d382e6955fc930b568
Nascosta <curtisj@knights.ucf.edu>
2020-07-06 16:19:09 -0400

    + Updated .gitignore to allow recipes_web/build to push (allowing deployment to Heroku) + Included .env file to prevent pre-flight check causing issues from differing versions of esLint in dependencies.

---
+++ recipes_web/build/precache-manifest.a6f2578c6c145517588ae7759a08a385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f471000f1465fd86fd28d7e7d8f3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d214c42eb01b2813fea4",
+    "url": "/static/js/2.a9f0422c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9f0422c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7ae12e7b91f9f34aaf",
+    "url": "/static/js/main.4688385e.chunk.js"
+  },
+  {
+    "revision": "329f4ea5cbf50ff26f2e",
+    "url": "/static/js/runtime-main.23ca76e7.js"
+  }
+]);
