e422ec26f1eeef8dd2c327c08fde63eb22210e06
Nascosta <curtisj@knights.ucf.edu>
2020-07-06 19:44:41 -0400

    Corrected earlier Mongo testing change.

--- api/UserAPI.js
+++ api/UserAPI.js
@@ -1,7 +1,7 @@
 const ObjectId = require('mongodb').ObjectId;
 
-function UserAPI(dbRef) {
-	this.db = dbRef;
+function UserAPI(clientRef) {
+	this.client = clientRef;
 }
 
 UserAPI.prototype.CreateUser = async function(req, res) {
