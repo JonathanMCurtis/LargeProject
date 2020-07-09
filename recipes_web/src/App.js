b02335c6de1cef50eec8de1a311a5d524cd24411
idelmr <idel_martinez@knights.ucf.edu>
2020-07-09 12:27:44 -0400

    Merge branch, add router store, and initial redux files

--- recipes_web/src/App.js
+++ recipes_web/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import { connect } from 'react-redux';
+import { createUser, loginUser } from './config/User';
 
 class App extends Component {
 	render() {
@@ -11,4 +13,7 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = { createUser, loginUser };
+const mapStateToProps = ({ loggedIn, currentUser }) => ({ loggedIn, currentUser });
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
