76d91d53d7949611e95af311bd5ed95c9e9f75be
d-dolorfino <37088081+d-dolorfino@users.noreply.github.com>
2020-07-09 00:22:17 -0400

    New landing page; modals working
    Need to fix overlay on background image.

--- recipes_web/src/Home.jsx
+++ recipes_web/src/Home.jsx
@@ -1,90 +1,95 @@
 import React, { Component } from 'react';
-import  NavigationBar  from "./components/NavigationBar";
-import { Login } from "./components/Forms";
-import { Signup } from "./components/Forms";
-import  Button  from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import './index.css';
+import NavigationBar from './components/NavigationBar';
+import { Login } from './components/Forms';
+import { Signup } from './components/Forms';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-class Home extends Component {    
-    constructor (props) {
-        super(props)
-        this.state = { visible: false }
-    }
+class Home extends Component {
+	constructor (props) {
+		super(props);
+		this.state = { loginModal: false, signupModal: false };
+	}
 
-    // Render functions for the modal forms
+	// Render functions for the modal forms
 
-    renderLoginModal () {
-        return (
-            <Modal centered id="login" show = { this.state.visible }
-                onHide = { () => this.setState({visible: false}) }
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Please log into your account:</Modal.Title>
-                </Modal.Header>
+	renderLoginModal () {
+		return (
+			<Modal centered show = { this.state.loginModal } onHide = { () => this.setState({ loginModal: false }) }>
+				<Modal.Header closeButton>
+					<Modal.Title>Please log into your account:</Modal.Title>
+				</Modal.Header>
 
-                <Modal.Body>
-                    <Login />
-                </Modal.Body>
+				<Modal.Body>
+					<Login />
+				</Modal.Body>
 
-                <Modal.Footer>
-                    <Button variant="success">Login</Button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
+				<Modal.Footer>
+					<Button variant = "success">Login</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
 
-    returnSignupModal () {
-        return (
-            <Modal centered id="signup" show = { this.state.visible }
-            onHide = { () => this.setState({visible: false}) }
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>Please log into your account:</Modal.Title>
-            </Modal.Header>
+	renderSignupModal () {
+		return (
+			<Modal centered show = { this.state.signupModal } onHide = { () => this.setState({ signupModal: false }) }>
+				<Modal.Header closeButton>
+					<Modal.Title>Please log into your account:</Modal.Title>
+				</Modal.Header>
 
-            <Modal.Body>
-                <Signup />
-            </Modal.Body>
+				<Modal.Body>
+					<Signup />
+				</Modal.Body>
 
-            <Modal.Footer>
-                <Button variant="success">Sign Up</Button>
-            </Modal.Footer>
-        </Modal>
-        );
-    }
+				<Modal.Footer>
+					<Button variant = "success">Sign Up</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
 
+	renderWelcomeText () {
+		return (
+			<div className = "text-white text-center mt-5">
+				<h1 className = "display-1">Welcome to Study Share!</h1>
+				<p className = "lead">A place to find share your notes and study with others!</p>
+			</div>
+		);
+	}
 
-    renderWelcomeText () {
-        return (
-            <div className="text-white text-center mt-5">
-                <h1 className="display-1">Welcome to Project Recipe!</h1>
-                <p className="lead">A place to find new recipes and share your own with others!</p>
-            </div>
-        );
-    }
+	renderButtons () {
+		return (
+			<div className = "d-flex justify-content-center">
+				<Button className = "mr-2"
+					variant = "primary"
+					size = "lg"
+					onClick = { () => this.setState({ loginModal: true }) }>Login
+				</Button>
+				<Button variant = "secondary"
+					size = "lg"
+					onClick = { () => this.setState({ signupModal: true }) }>Sign Up
+				</Button>
+			</div>
+		);
+	}
 
-    render () {
-        return (
-            <div>
-                <NavigationBar />
-                { this.renderLoginModal() }
-                {/* { this.renderSignupModal() } */}
-                { this.renderWelcomeText() }
+	render () {
+		return (
+			<div>
+				<NavigationBar />
 
-                <div className="d-flex justify-content-center">
-                    <Button className="mr-2" 
-                            variant="primary" 
-                            size="lg"
-                            onClick={ () => this.setState({visible:true}) }>Login
-                    </Button>
-                    <Button variant="secondary" 
-                            size="lg"
-                            onClick="">Sign Up
-                    </Button>
-            </div>
-            </div>
-        );
-    }
-  }
-  
-  export default Home;
+				{ this.renderLoginModal() }
+				{ this.renderSignupModal() }
+
+				<div className = 'mh-100'>
+					{ this.renderWelcomeText() }
+					{ this.renderButtons() }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
--- recipes_web/src/components/NavigationBar.jsx
+++ recipes_web/src/components/NavigationBar.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { Component } from "react";
-import Nav from "react-bootstrap/Nav";
-import NavBar from "react-bootstrap/Navbar";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavBar from 'react-bootstrap/Navbar';
 
 // Navigation bar for index page
 class NavigationBar extends Component {
-    render () {
-        return (
-            <NavBar bg="dark" variant="dark">
-            <NavBar.Brand href="#home">Recipe Project</NavBar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About Us</Nav.Link>
-                <Nav.Link href="#contact">Contact Us</Nav.Link>
-            </Nav>
-        </NavBar>
-        );
-    }
+	render () {
+		return (
+			<NavBar bg = "dark" variant = "dark" fixed = "top">
+				<NavBar.Brand href = "#home">Study Share</NavBar.Brand>
+				<Nav className = "ml-auto">
+					<Nav.Link href = "#home">Home</Nav.Link>
+					<Nav.Link href = "#about">About Us</Nav.Link>
+					<Nav.Link href = "#contact">Contact Us</Nav.Link>
+				</Nav>
+			</NavBar>
+		);
+	}
 }
 
 export default NavigationBar;
