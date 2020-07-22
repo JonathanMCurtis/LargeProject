836f81cd77db4d14748b1ff487989334120cafad
d-dolorfino <37088081+d-dolorfino@users.noreply.github.com>
2020-07-22 17:49:51 -0400

    Update styles on Note.js

--- recipes_web/src/pages/Notes.js
+++ recipes_web/src/pages/Notes.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { NavBar } from '../components';
 import { connect } from 'react-redux';
 import { getNotes } from '../config';
-import Container from 'react-bootstrap/Container';
+import { Container, Badge } from 'react-bootstrap';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
 import './styles.css';
 
-const NoteCard = ({ noteID, title, submissionDate, favoriteCount }) => {
+const NoteCard = ({ noteID, author, title, submissionDate, favoriteCount }) => {
 	return (
-		<button className = 'noteCard'>
-			<h5>
+		<button className = 'noteCard m-4 bg-light'>
+			<h5 className = 'text-dark'>
 				{ title }
 			</h5>
 			<div>
-				{ submissionDate }
+				<p className = 'text-secondary'>Author: <span className = 'text-dark'>{ author }</span></p>
+				<p className = 'text-secondary'>Made on: <span className = 'text-dark'>{ submissionDate }</span></p>
+				<div className = 'float-right'>
+					<FaRegBookmark className = 'text-primary' />
+					<Badge variant = 'primary'>{ favoriteCount }</Badge>
+				</div>
 			</div>
 		</button>
 	);
@@ -27,20 +33,27 @@ class Notes extends Component {
 
 	render () {
 		const { notes } = this.props;
+		const { getNotes, router: { match: { params: { subject, topic } } } } = this.props;
 
 		const notesArr = [
-			{ title: 'Integrals', submissionDate: '7/22/2020', favoriteCount: 10 }
+			{ title: 'Integrals', author: 'Alice', submissionDate: '7/22/2020', favoriteCount: 10 },
+			{ title: 'Limits', author: 'Bob', submissionDate: '7/20/2020', favoriteCount: 7 },
+			{ title: 'Other Calculus things', author: 'Cole', submissionDate: '7/20/2020', favoriteCount: 21 },
+			{ title: 'Even more Calculus things', author: 'Danny', submissionDate: '7/20/2020', favoriteCount: 21 }
 		];
 
 		return (
-			<div className = 'bg-primary'>
+			<div className = 'notes-bg pattern-grid-lg bg-secondary text-light'>
 				<NavBar />
-				<Container>
-					{ notesArr.map(note => {
-						return (
-							NoteCard(note)
-						);
-					}) }
+				<Container className = 'h-100 justify-content-md-center'>
+					<div className = 'notes-container'>
+						<h1 className = 'display-1 text-success text-center'>{ topic }</h1>
+						{ notesArr.map(note => {
+							return (
+								NoteCard(note)
+							);
+						}) }
+					</div>
 				</Container>
 			</div>
 		);
